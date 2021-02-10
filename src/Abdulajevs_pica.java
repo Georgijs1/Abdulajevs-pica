013d82838eda6fc0960b0d5eb814b99c0799e802
Georgijs <lakawije@gmail.com>
2021-02-10 23:59:34 +0200

    Programmas otrā versija: Scanner'a un metožu pievienošana

--- src/Abdulajevs_pica.java
+++ src/Abdulajevs_pica.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
 
-public class Abdulajevs_pica {
-
-	public static void main(String[] args) {
+public class Abdulajevs_pica{
+	static Scanner dati = new Scanner(System.in);
+	
+	public static void majas(){
 		
+	}
+	
+	public static void uzvietas(){
+		
+	}
+	
+	public static void saglabat(){
+		
+	}
+	
+	public static void izvadit(){
+		
+	}
+	
+	public static void main(String[] args){
+	
 
 	}
 
