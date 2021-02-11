f275f28d713d4d74340a682fd1af57a2c4bd9b98
Georgijs <lakawije@gmail.com>
2021-02-11 20:30:13 +0200

    Programmas astotā versija: FileWriter un FileReader pivienošana, kā arī neliela koda rediģēšana

--- src/Abdulajevs_pica.java
+++ src/Abdulajevs_pica.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.text.DecimalFormat;
@@ -12,8 +14,8 @@ public class Abdulajevs_pica{
 	static Scanner dati = new Scanner(System.in);
 	
 	public static void majas() throws Exception{
-		String uzvards, talrunis, adrese;
-		double attalums=0, piegadesmaksa=0, izmeracena=0, tipacena=0, mercescena=0;
+		String uzvards, talrunis, adrese, teksts;
+		double piegadesmaksa=0, izmeracena=0, tipacena=0, mercescena=0, summa;
 		
 		JOptionPane.showMessageDialog(null, "Jūs izvēlaties piegādi uz māju.");
 		uzvards = (JOptionPane.showInputDialog("Ievadiet savu uzvārdu: "));
@@ -37,60 +39,86 @@ public class Abdulajevs_pica{
         		null, new Object[] {"Ananāsu", "Peperoni", "Sēņu", "Veģetārā"}, JOptionPane.YES_OPTION);
 
         	if(picastips==JOptionPane.YES_OPTION){
-        		tipacena=5.50;
+        		tipacena=tipacena + 5.50;
         	}else if(picastips==JOptionPane.NO_OPTION){
-        		tipacena=5.00;
+        		tipacena=tipacena + 5.00;
         	}else if(picastips==JOptionPane.OK_OPTION){
-        		tipacena=4.50;
+        		tipacena=tipacena + 4.50;
         	}else if(picastips==JOptionPane.CANCEL_OPTION){
-        		tipacena=4.00;
+        		tipacena=tipacena + 4.00;
         	}
         
         int izmers = JOptionPane.showOptionDialog(new JFrame(), "Kāda izmēra picu Jūs vēlaties piegādāties?", "Picas izmērs", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, 
         		null, new Object[] {"25cm", "30cm", "50cm"}, JOptionPane.YES_OPTION);
         	
         	if(izmers==JOptionPane.YES_OPTION){
-        		izmeracena = tipacena * 0.2;
+        		izmeracena = tipacena * 0.2 + izmeracena;
         	}else if(izmers==JOptionPane.NO_OPTION){
-        		izmeracena = tipacena * 0.4;
+        		izmeracena = tipacena * 0.4 + izmeracena;
         	}else if(izmers==JOptionPane.OK_OPTION){
-        		izmeracena = tipacena * 0.6;
+        		izmeracena = tipacena * 0.6 + izmeracena;
         	} 
         	
         	int merce = JOptionPane.showOptionDialog(new JFrame(), "Vai velaties piegādāties papildus mērci?", "Papildus mērce", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, 
             		null, new Object[] {"Ketčups", "Sinapju", "Majonēze"}, JOptionPane.YES_OPTION);
         	
         	if(izmers==JOptionPane.YES_OPTION){
-        		mercescena = 0.60;
+        		mercescena = mercescena + 0.60;
         	}else if(izmers==JOptionPane.NO_OPTION){
-        		mercescena = 0.80;
+        		mercescena = mercescena + 0.80;
         	}else if(izmers==JOptionPane.OK_OPTION){
-        		mercescena = 1.00;
+        		mercescena = mercescena + 1.00;
         	} 
         	
+        	summa=tipacena+izmeracena+mercescena+piegadesmaksa;
+        			
 			try{
 			
 				FileWriter fw = new FileWriter("Pasutijums.txt", true);
 				PrintWriter raksta = new PrintWriter(fw);
 				
+				raksta.println("");
 				raksta.println("Info par klientu: ");
-				raksta.println("Klienta vārds: "+uzvards);
+				raksta.println("Klienta uzvārds: "+uzvards);
 				raksta.println("Klienta adrese: "+adrese);
-				raksta.println("Attālums no picērijas: "+attalums);
 				raksta.println("Klienta tālrunis: "+talrunis);
+				raksta.println("");
+				raksta.println("Jūsu rēķins: ");
+				raksta.println("Jūsu picas cena: "+tipacena);
+				raksta.println("Сena par Jūsu izvēlēto picas izmēru izmēru: "+izmeracena);
+				raksta.println("Jūsu mērces cena: "+mercescena);
 				raksta.println("Jūsu piegādes maksa būs: "+df.format(piegadesmaksa)+" euro");
-				raksta.println("Jūsu picas izmērs: "+izmers);
-				raksta.println("Cēna par izmēru: "+izmeracena);
-				raksta.println("Jūsu mērce: "+merce);
-				raksta.println("Cēna par mērci: "+mercescena);
+				raksta.println("Cena kopā: "+summa+" eiro");	
 			
-				JOptionPane.showMessageDialog(null, "Jūsu pasūtījums tiek saņemts!");
+				JOptionPane.showMessageDialog(null, "Jūsu pasūtījums tiek saņemts veiksmīgi!", "Paldies par pasūtījumu!", JOptionPane.WARNING_MESSAGE);
 				raksta.close();
 				
 			}catch(Exception e){
 				JOptionPane.showMessageDialog(null, "Kļūme ierakstot failā", "Kļūme!", JOptionPane.ERROR_MESSAGE);	
 		}
-	}	
+			
+			UIManager.put("OptionPane.yesButtonText", "Jā");
+			UIManager.put("OptionPane.noButtonText", "Nē");
+	        int apskatit = JOptionPane.showConfirmDialog(null, "Vai velaties apskatīties Jūsu pasūtījumu?", "Pasūtījums", JOptionPane.YES_NO_OPTION);  
+	        
+	        	if(apskatit== JOptionPane.YES_OPTION){
+	        		try{
+	    				
+	    				FileReader fr = new FileReader("Pasutijums.txt");
+	    				BufferedReader lasa = new BufferedReader(fr);
+	    				
+	    				while((teksts=lasa.readLine())!=null){
+	    					System.out.println(teksts);
+	    				}
+	    				lasa.close();
+	    				
+	    			}catch(Exception e){
+	    				JOptionPane.showMessageDialog(null, "Kļūme nolasot failu!", "Kļūme!", JOptionPane.ERROR_MESSAGE);
+	    			}
+	        	}if(apskatit == JOptionPane.NO_OPTION){
+	        		JOptionPane.showMessageDialog(null, "Paldies par Jūsu pasūtījumu!");
+	        	}
+	}
 	
 	public static void uzvietas(){
 		String uzvards, talrunis, adrese;
